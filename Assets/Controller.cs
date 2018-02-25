f05c65cd2a6f9db30a5e05d8032ebd2bd8a9edfd
Kim-jasper B Mui <kmui@UCSD.EDU>
2018-02-24 16:53:53 -0800

    Set up prefabs

---
+++ Assets/Controller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+    public GameObject Cube;
+    private GameObject cube;
+
+
+	// Use this for initialization
+	void Start () {
+
+        PlacePosition();		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void PlacePosition()
+    {
+
+        cube = Instantiate(Cube);
+        cube.transform.position = new Vector3(1.0f, 1.0f, 1.0f);
+    }
+}
---
+++ Assets/Prefabs/GenerateWalls.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateWalls : MonoBehaviour {
+
+    public GameObject Wall;
+    private GameObject wall;
+
+    public const int WALL_SIZE = 10;
+    public const double HALF = 2.0;
+
+	// Use this for initialization
+	void Start () {
+
+        RandomlyGenerateWalls();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    private void RandomlyGenerateWalls()
+    {
+        wall = Instantiate(Wall);
+        wall.transform.position = new Vector3(5.0f, 5.0f, 0.0f);
+    }
+}
